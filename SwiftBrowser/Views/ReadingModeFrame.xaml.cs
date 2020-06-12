2367cafb788a7712e98b571c2f25b37cc611839d
FireCubeStudios <ayushmaan.b27@outlook.com>
2020-06-12 23:13:28 +0200

    added fonts and text size for reading mode

--- SwiftBrowser/Views/ReadingModeFrame.xaml.cs
+++ SwiftBrowser/Views/ReadingModeFrame.xaml.cs
@@ -25,12 +25,113 @@ namespace SwiftBrowser.Views
         public static string TitleString { get; set; }
         public static string ImageString { get; set; }
         public static string BodyString { get; set; }
+        public List<Tuple<string, FontFamily>> Fonts { get; } = new List<Tuple<string, FontFamily>>()
+
+            {
+
+                new Tuple<string, FontFamily>("Arial", new FontFamily("Arial")),
+
+                new Tuple<string, FontFamily>("Comic Sans MS", new FontFamily("Comic Sans MS")),
+
+                new Tuple<string, FontFamily>("Courier New", new FontFamily("Courier New")),
+
+                new Tuple<string, FontFamily>("Segoe UI", new FontFamily("Segoe UI")),
+
+                new Tuple<string, FontFamily>("Times New Roman", new FontFamily("Times New Roman"))
+
+            };
+        public List<double> FontSizes { get; } = new List<double>()
+
+            {
+
+                8,
+
+                9,
+
+                10,
+
+                11,
+
+                12,
+
+                14,
+
+                16,
+
+                18,
+
+                20,
+
+                24,
+
+                28,
+
+                36,
+
+                48,
+
+                72
+
+            };
         public ReadingModeFrame()
         {
             this.InitializeComponent();
             Title.Text = TitleString;
             // ImageArticle.Source = new
             RichBodyText.Text = BodyString;
+        }
+        private void Combo3_TextSubmitted(ComboBox sender, ComboBoxTextSubmittedEventArgs args)
+
+        {
+
+            bool isDouble = double.TryParse(sender.Text, out double newValue);
+
+
+
+            // Set the selected item if:
+
+            // - The value successfully parsed to double AND
+
+            // - The value is in the list of sizes OR is a custom value between 8 and 100
+
+            if (isDouble && (FontSizes.Contains(newValue) || (newValue < 100 && newValue > 8)))
+
+            {
+
+                // Update the SelectedItem to the new value. 
+
+                sender.SelectedItem = newValue;
+
+            }
+
+            else
+
+            {
+
+                // If the item is invalid, reject it and revert the text. 
+
+                sender.Text = sender.SelectedValue.ToString();
+
+
+
+                var dialog = new ContentDialog();
+
+                dialog.Content = "The font size must be a number between 8 and 100.";
+
+                dialog.CloseButtonText = "Close";
+
+                dialog.DefaultButton = ContentDialogButton.Close;
+
+                var task = dialog.ShowAsync();
+
+            }
+
+
+
+            // Mark the event as handled so the framework doesn’t update the selected item automatically. 
+
+            args.Handled = true;
+
         }
     }
 }
